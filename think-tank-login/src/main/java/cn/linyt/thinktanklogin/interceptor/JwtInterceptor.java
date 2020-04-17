360ddd1e49c22aa4fa975197fe543ad4596c5cf6
Mojo <1316578052@qq.com>
2020-04-18 01:36:36 +0800

    22

--- think-tank-login/src/main/java/cn/linyt/thinktanklogin/interceptor/JwtInterceptor.java
+++ think-tank-login/src/main/java/cn/linyt/thinktanklogin/interceptor/JwtInterceptor.java
@@ -67,7 +67,8 @@ public class JwtInterceptor extends HandlerInterceptorAdapter {
             log.info("### User is not logged in, please log in first ###");
             //设置响应状态码
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.setCharacterEncoding("utf-8");
+//            response.setCharacterEncoding("utf-8");
+            response.setContentType("application/json; charset=utf-8");
             Result result = new Result(ResultCode.USER_NOT_LOGGED_IN);
             log.info("### 响应体数据：" + result + " ###");
             response.getWriter().write(JSONObject.toJSONString(result));
