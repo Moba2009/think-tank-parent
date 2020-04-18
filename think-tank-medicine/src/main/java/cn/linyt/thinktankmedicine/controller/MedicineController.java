8bebbb7ae052792056fde57c950d121cfacfa748
Mojo <1316578052@qq.com>
2020-04-19 02:04:43 +0800

    27

--- think-tank-medicine/src/main/java/cn/linyt/thinktankmedicine/controller/MedicineController.java
+++ think-tank-medicine/src/main/java/cn/linyt/thinktankmedicine/controller/MedicineController.java
@@ -35,24 +35,40 @@ public class MedicineController {
     @Autowired
     private MedicineRepository medicineRepository;
 
+    /**
+     * @Description TODO    GET /medicines
+     * @Author Mojo
+     * @Date 2020/4/19 1:54
+     **/
     @GetMapping
     public List<MedicinePro> list(HttpServletResponse response) {
 
         //获取药材列表
         log.info("### get medicines ###");
         List<MedicinePro> medicineProList = new ArrayList<>();
-        List<Medicine> list;
+        List<Medicine> children;
         String type;
         for (int i = 1; i <= 26; i++) {
             type = String.valueOf(Character.toUpperCase((char) (96 + i)));
-            list = medicineRepository.findByType(type);
-            if (list.isEmpty())
+            children = medicineRepository.findByType(type);
+            if (children.isEmpty())
                 continue;
             else
-                medicineProList.add(new MedicinePro(type, list));
+                medicineProList.add(new MedicinePro(type, children));
         }
         //设置响应数据类型和编码方式
         response.setContentType("application/json; charset=utf-8");
         return medicineProList;
     }
+
+    /**
+     * @Description TODO    GET /medicines/
+     * @Author Mojo
+     * @Date 2020/4/19 2:01
+     **/
+//    @GetMapping
+//    public Medicine detail() {
+//
+//
+//    }
 }
