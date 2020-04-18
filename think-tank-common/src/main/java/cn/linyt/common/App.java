ce7f61d23b691b607af64a7b0b4b47ad2b63b55d
Mojo <1316578052@qq.com>
2020-04-19 04:07:26 +0800

    34

---
+++ think-tank-common/src/main/java/cn/linyt/common/App.java
@@ -0,0 +1,15 @@
+package cn.linyt.common;
+
+/**
+ * @ClassName App
+ * @Description TODO
+ * @Author Mojo
+ * @Date 2020/4/19 4:07
+ * @Version 1.0
+ **/
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
