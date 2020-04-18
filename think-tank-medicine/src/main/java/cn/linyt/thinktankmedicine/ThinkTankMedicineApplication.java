7ac77efcefe6c47b27b1140fa99b0f9e104c61fd
Mojo <1316578052@qq.com>
2020-04-18 16:12:53 +0800

    24

---
+++ think-tank-medicine/src/main/java/cn/linyt/thinktankmedicine/ThinkTankMedicineApplication.java
@@ -0,0 +1,13 @@
+package cn.linyt.thinktankmedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkTankMedicineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkTankMedicineApplication.class, args);
+    }
+
+}
