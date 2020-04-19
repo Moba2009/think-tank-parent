45b944b51b09c309373d1f769da256370b19ee04
Mojo <1316578052@qq.com>
2020-04-20 07:09:23 +0800

    50

--- think-tank-common/src/main/java/cn/linyt/common/entity/User.java
+++ think-tank-common/src/main/java/cn/linyt/common/entity/User.java
@@ -2,6 +2,11 @@ package cn.linyt.common.entity;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @ClassName User
  * @Description TODO
@@ -10,8 +15,12 @@ import lombok.Data;
  * @Version 1.0
  **/
 @Data
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String username;
     private String password;
 }
