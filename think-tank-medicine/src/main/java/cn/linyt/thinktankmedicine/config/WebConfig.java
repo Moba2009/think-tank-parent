f2c0b51d8904948c8859682b158c3829261c8e50
Mojo <1316578052@qq.com>
2020-04-21 00:56:47 +0800

    68

--- think-tank-medicine/src/main/java/cn/linyt/thinktankmedicine/config/WebConfig.java
+++ think-tank-medicine/src/main/java/cn/linyt/thinktankmedicine/config/WebConfig.java
@@ -31,7 +31,7 @@ public class WebConfig implements WebMvcConfigurer {
      *
      * @param registry
      */
-    /*@Override
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
@@ -39,5 +39,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS", "HEAD")
                 .maxAge(3600 * 24);
-    }*/
+    }
 }
